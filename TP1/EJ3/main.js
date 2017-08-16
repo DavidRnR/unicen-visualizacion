3f901dc59f2e3e0a3f7b7e4bf6a389c6d750b9c7
David <carlosdavidmartin@gmail.com>
2017-08-16 17:19:15 -0300

    EJ done

--- TP1/EJ3/main.js
+++ TP1/EJ3/main.js
@@ -1,31 +1,40 @@
 var ctx = document.getElementById("canvas").getContext("2d");
 
-var width = 200;
-var height = 400;
+var width = 500;
+var height = 600;
 
 var imageData = ctx.createImageData(width, height);
 
 var r = 0;
 var g = 0;
 var b = 0;
+var a = 255; 
 
 for (var x = 0; x < width; x++) {
     
     for(var y = 0; y < height; y++) {
-        setPixel(imageData, x, y, r , g, b, 255);
-      
+        setPixel(imageData, x, y, r , g, b, a);
+        r = getColorVal(y);
+        g = getColorVal(y);
+        b = getColorVal(y);
     }
-        r++;
-        g++;
-        b++;
+    r = 0;
+    g = 0;
+    b = 0;
+}
+
+function getColorVal(y) {
+    return Math.floor((y/height) * 255);
 }
 
 ctx.putImageData(imageData, 0, 0);
 
 function setPixel(imageData, x, y, r, g, b, a) {
+
     index = (x + y * imageData.width) * 4;
     imageData.data[index+0] = r;
     imageData.data[index+1] = g;
     imageData.data[index+2] = b;
     imageData.data[index+3] = a;
+     
 }
