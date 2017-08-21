0bd40c92908f55ef1bea61880db2eedafad953eb
David Martín <carlosdavidmartin@gmail.com>
2017-08-21 01:10:41 -0300

    Logo y render image processed

---
+++ TP1/EntregaFinal/Readme.md
@@ -0,0 +1,7 @@
+# TUDAI TP1 Entrega Final
+
+APP sobre procesado de imagenes.
+
+![alt text](https://github.com/DavidRnR/unicen-visualizacion/tree/master/TP1/EntregaFinal/img/logoTudaiPicFilter.png)
+
+## Martín, Calos David
--- TP1/EntregaFinal/js/main.js
+++ TP1/EntregaFinal/js/main.js
@@ -4,8 +4,17 @@ var ctxNegative = null;
 var ctxBinary = null;
 var ctxSepia = null;
 
+var imageOrigin = new Image();
+
+/**
+ * On load page, render the menu
+ */
 renderHtml('./menu.html');
 
+/**
+ * Render HTML
+ * @param {url} url 
+ */
 function renderHtml(url) {
     var headers = new Headers();
     headers.append("Content-Type", "text/html");
@@ -17,11 +26,17 @@ function renderHtml(url) {
             switch (url) {
                 case './menu.html':
                     document.getElementById('app-loader').innerHTML = data;
+                    setImageFromInput();
                     break;
-                case './image-process.html':
+                case './image-processed.html':
                     document.getElementById('app-loader').innerHTML = data;
-                    onSetCanvas();
+                    onSetCanva();
                     onLoadImage();
+                    break;
+                case './image-filters.html':
+                    document.getElementById('filters').innerHTML = data;
+                    onSetCanvasFilters();
+                    onLoadImagesFilters();
                     break;
                 default:
                     break;
@@ -29,10 +44,42 @@ function renderHtml(url) {
         }).catch(err => console.log(err));
 }
 
+/**
+ * On Set Image from Input
+ */
+function setImageFromInput() {
+    var imageFile = function () {
+        var fileInput = document.querySelector("#uploadImage");
 
-function onSetCanvas() {
+        // File Reader and set SRC
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            imageOrigin.src = e.target.result;
+        };
+        reader.readAsDataURL(fileInput.files[0]);
+
+        // Render Image Processed
+        renderHtml('./image-processed.html');
+
+         // Render Image Filters
+         renderHtml('./image-filters.html');
+    }
+    document.querySelector("#uploadImage").onchange = imageFile;
+
+}
+
+/**
+ * Set Canvas 
+ */
+function onSetCanva() {
     // Main Canvas
     ctx = document.getElementById("canvas").getContext("2d");
+}
+
+/**
+ * Set Canvas 
+ */
+function onSetCanvasFilters() {
 
     // Canvas Filters
     ctxBW = document.getElementById("canvas-black-white").getContext("2d");
@@ -40,9 +87,11 @@ function onSetCanvas() {
     ctxBinary = document.getElementById("canvas-binary").getContext("2d");
     ctxSepia = document.getElementById("canvas-sepia").getContext("2d");
 }
+
+/**
+ * On Load Image
+ */
 function onLoadImage() {
-    var imageOrigin = new Image();
-    imageOrigin.src = "image1.jpg";
 
     imageOrigin.onload = function () {
         ctx.drawImage(this, 0, 0);
@@ -50,33 +99,38 @@ function onLoadImage() {
         imageData = ctx.getImageData(0, 0, this.width, this.height);
 
         ctx.putImageData(imageData, 0, 0);
-    }
-
-
-    var imageBlackWhite = new Image();
-    imageBlackWhite.src = "image1.jpg";
-
-    imageBlackWhite.onload = function () {
-
-        var w = imageBlackWhite.width;
-        var h = imageBlackWhite.height;
-
-        var sizer = scalePreserveAspectRatio(w, h, 120, 160);
-
-        ctxBW.drawImage(this, 0, 0, w, h, 0, 0, w * sizer, h * sizer);
-
-        imageData = ctxBW.getImageData(0, 0, this.width, this.height);
-
-        getFilterBlackWhite(imageData);
-
-        ctxBW.canvas.width = w * sizer;
-        ctxBW.canvas.height = h * sizer;
-
-        ctxBW.putImageData(imageData, 0, 0);
-    }
+    }    
 
 }
 
+/**
+* On Load Images Filters
+*/
+function onLoadImagesFilters() {
+
+   var imageBlackWhite = new Image();
+   imageBlackWhite.src = imageOrigin.src;
+
+   imageBlackWhite.onload = function () {
+
+       var w = imageBlackWhite.width;
+       var h = imageBlackWhite.height;
+
+       var sizer = scalePreserveAspectRatio(w, h, 120, 160);
+
+       ctxBW.drawImage(this, 0, 0, w, h, 0, 0, w * sizer, h * sizer);
+
+       imageData = ctxBW.getImageData(0, 0, this.width, this.height);
+
+       getFilterBlackWhite(imageData);
+
+       ctxBW.canvas.width = w * sizer;
+       ctxBW.canvas.height = h * sizer;
+
+       ctxBW.putImageData(imageData, 0, 0);
+   }
+
+}
 
 function onSetFilter(filter) {
 
@@ -110,7 +164,6 @@ function setPixel(imageData, x, y, r, g, b, a) {
     imageData.data[index + 3] = a;
 
 }
-
 
 
 function scalePreserveAspectRatio(imgW, imgH, maxW, maxH) {
