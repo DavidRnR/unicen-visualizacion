b8e20fc50f1b1965348eee13e6c3ff5408f35d4c
David Martín <carlosdavidmartin@gmail.com>
2017-08-20 23:30:39 -0300

    Entrega Final Render fetch html

--- TP1/EntregaFinal/js/main.js
+++ TP1/EntregaFinal/js/main.js
@@ -1,10 +1,105 @@
-var ctx = document.getElementById("canvas").getContext("2d");
+var ctx = null;
+var ctxBW = null;
+var ctxNegative = null;
+var ctxBinary = null;
+var ctxSepia = null;
 
-// Canvas Filters
-var ctxBW = document.getElementById("canvas-black-white").getContext("2d");
-var ctxNegative = document.getElementById("canvas-negative").getContext("2d");
-var ctxBinary = document.getElementById("canvas-binary").getContext("2d");
-var ctxSepia = document.getElementById("canvas-sepia").getContext("2d");
+renderHtml('./menu.html');
+
+function renderHtml(url) {
+    var headers = new Headers();
+    headers.append("Content-Type", "text/html");
+
+    fetch(url, headers)
+        .then(response => {
+            return response.text();
+        }).then(data => {
+            switch (url) {
+                case './menu.html':
+                    document.getElementById('app-loader').innerHTML = data;
+                    break;
+                case './image-process.html':
+                    document.getElementById('app-loader').innerHTML = data;
+                    onSetCanvas();
+                    onLoadImage();
+                    break;
+                default:
+                    break;
+            }
+        }).catch(err => console.log(err));
+}
+
+
+function onSetCanvas() {
+    // Main Canvas
+    ctx = document.getElementById("canvas").getContext("2d");
+
+    // Canvas Filters
+    ctxBW = document.getElementById("canvas-black-white").getContext("2d");
+    ctxNegative = document.getElementById("canvas-negative").getContext("2d");
+    ctxBinary = document.getElementById("canvas-binary").getContext("2d");
+    ctxSepia = document.getElementById("canvas-sepia").getContext("2d");
+}
+function onLoadImage() {
+    var imageOrigin = new Image();
+    imageOrigin.src = "image1.jpg";
+
+    imageOrigin.onload = function () {
+        ctx.drawImage(this, 0, 0);
+
+        imageData = ctx.getImageData(0, 0, this.width, this.height);
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+
+    var imageBlackWhite = new Image();
+    imageBlackWhite.src = "image1.jpg";
+
+    imageBlackWhite.onload = function () {
+
+        var w = imageBlackWhite.width;
+        var h = imageBlackWhite.height;
+
+        var sizer = scalePreserveAspectRatio(w, h, 120, 160);
+
+        ctxBW.drawImage(this, 0, 0, w, h, 0, 0, w * sizer, h * sizer);
+
+        imageData = ctxBW.getImageData(0, 0, this.width, this.height);
+
+        getFilterBlackWhite(imageData);
+
+        ctxBW.canvas.width = w * sizer;
+        ctxBW.canvas.height = h * sizer;
+
+        ctxBW.putImageData(imageData, 0, 0);
+    }
+
+}
+
+
+function onSetFilter(filter) {
+
+    imageData = ctx.getImageData(0, 0, imageOrigin.width, imageOrigin.height);
+
+    switch (filter) {
+        case 'bwFilter':
+            getFilterBlackWhite(imageData);
+            break;
+        case 'negativeFilter':
+            getFilterNegative(imageData);
+            break;
+        case 'sepiaFilter':
+            getFilterSepia(imageData);
+            break;
+        case 'binaryFilter':
+            getFilterBinary(imageData);
+            break;
+        default:
+            break;
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
 
 function setPixel(imageData, x, y, r, g, b, a) {
 
@@ -16,60 +111,11 @@ function setPixel(imageData, x, y, r, g, b, a) {
 
 }
 
-var imageOrigin = new Image();
-imageOrigin.src = "image1.jpg";
 
-imageOrigin.onload = function () { 
-    ctx.drawImage(this, 0, 0);
 
-    imageData = ctx.getImageData(0, 0, this.width, this.height);
-    // getFilterBlackWhite(imageData);
-    // getFilterNegative(imageData);
-    // getFilterSepia(imageData);
-    // getFilterBinary(imageData);
-    ctx.putImageData(imageData, 0, 0);
+function scalePreserveAspectRatio(imgW, imgH, maxW, maxH) {
+    return (Math.min((maxW / imgW), (maxH / imgH)));
 }
-
-function onSetFilter(filter) {
-
-        imageData = ctx.getImageData(0, 0, imageOrigin.width, imageOrigin.height);
-
-        switch (filter) {
-            case 'bw':
-                getFilterBlackWhite(imageData);
-                break;
-            default:
-                break;
-        }     
-        ctx.putImageData(imageData, 0, 0);
-}
-
-
-var imageBlackWhite = new Image();
-imageBlackWhite.src = "image1.jpg";
-
-imageBlackWhite.onload = function () {
-
-    var w = imageBlackWhite.width;
-    var h = imageBlackWhite.height;
-
-    var sizer = scalePreserveAspectRatio(w,h,120,160);
-
-    ctxBW.drawImage(this,0,0,w,h,0,0,w*sizer,h*sizer);
-
-    imageData = ctxBW.getImageData(0, 0, this.width, this.height);
-  
-    getFilterBlackWhite(imageData);
- 
-    ctxBW.canvas.width = w*sizer;
-    ctxBW.canvas.height = h*sizer;
-
-    ctxBW.putImageData(imageData, 0, 0);
-}
-
-function scalePreserveAspectRatio(imgW,imgH,maxW,maxH){
-    return(Math.min((maxW/imgW),(maxH/imgH)));
-  }
 
 function getRed(imageData, x, y) {
     index = (x + y * imageData.width) * 4;
