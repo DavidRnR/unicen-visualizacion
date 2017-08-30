2cd7a7f86317a68f860a8151d99fe8e0da5d315b
Terminal 4 VDI Lab III <carlosdavidmartin@gmail.com>
2017-08-30 17:39:47 -0300

    EJX

--- TP2/EJX/main.js
+++ TP2/EJX/main.js
@@ -1,15 +1,70 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-console.log(canvas);
-console.log(ctx);
+
+var BB = canvas.getBoundingClientRect();
+var offsetX = BB.left;
+var offsetY = BB.top;
+
+var startX;
+var startY;
+
+var dragAndDrop = false;
+
 canvas.onclick = function () {
 
-  c.draw();
+  // c.draw();
 };
 
+// canvas.onmousedown = function (e) {
+//   let pos =  Math.sqrt(Math.pow((e.clientX - c.posX),2) + Math.pow((e.clientY - c.posY),2));
+//   console.log(pos);
+// }
+
 canvas.onmousedown = function (e) {
-  let pos =  Math.sqrt(Math.pow((e.clientX - c.posX),2) + Math.pow((e.clientY - c.posY),2));
-  console.log(pos);
+  e.preventDefault();
+  e.stopPropagation();
+
+  // get the current mouse position
+  var mx = parseInt(e.clientX - offsetX);
+  var my = parseInt(e.clientY - offsetY);
+
+  dragAndDrop = true;
+  // save the current mouse position
+  startX = mx;
+  startY = my;
+
+};
+
+canvas.onmouseup = function (e) {
+  // tell the browser we're handling this mouse event
+  e.preventDefault();
+  e.stopPropagation();
+  dragAndDrop = false;
+  canvas.onmousemove = null;
+};
+
+function myMove (e) {
+
+  if(dragAndDrop) {
+    // get the current mouse position
+    var mx = parseInt(e.clientX - offsetX);
+    var my = parseInt(e.clientY - offsetY);
+    // calculate the distance the mouse has moved
+    // since the last mousemove
+    var dx = mx - startX;
+    var dy = my - startY;
+
+    c.posX += dx;
+    c.posY += dy;
+
+    // redraw the scene with the new rect positions
+    draw();
+
+    // reset the starting mouse position for the next mousemove
+    startX = mx;
+    startY = my;
+  }
+
 }
 
 // canvas.addEventListener('mousemove', function(evt) {
@@ -56,3 +111,5 @@ c.posX = Math.floor((Math.random() * 900) + 1);
 c.posY = Math.floor((Math.random() * 600) + 1);
 c.radio = 70;
 c.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+c.draw();
