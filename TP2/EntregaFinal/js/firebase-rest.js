5d78d804f09b9a5394584c9f715f2998435643f3
David Martín <dmartin@beerealit.com>
2017-09-12 14:49:21 -0300

    Hanoi Towers finished

--- TP2/EntregaFinal/js/firebase-rest.js
+++ TP2/EntregaFinal/js/firebase-rest.js
@@ -1,94 +1,51 @@
 var onSubmitScore = null;
 
-var firebaseUrl = "https://towers-of-hanoi-8c023.firebaseio.com/scores";
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCARvu_Io6LWJkgbAV7Qmirk0TDtCiXoNY",
+    authDomain: "towers-of-hanoi-8c023.firebaseapp.com",
+    databaseURL: "https://towers-of-hanoi-8c023.firebaseio.com",
+    projectId: "towers-of-hanoi-8c023",
+    storageBucket: "towers-of-hanoi-8c023.appspot.com",
+    messagingSenderId: "67527398432"
+};
+firebase.initializeApp(config);
 
 /**
  * Firebase Get Scores
  */
-function getScores() {
-
-    // Loading Icon
-    document.getElementsByClassName('hanoi-score-list')[0].innerHTML = '<i class="fa fa-spinner fa-2x fa-spin"></i>';
-
-    // Headers
-    let headers = new Headers();
-    headers.append("Content-Type", "application/json");
-
-    let options = {
-        headers: headers,
-        cache: 'default'
-    };
-
-    // Build endpoint
-    let endpoint = firebaseUrl + "/" + hanoiTowersGame.quantDisks +".json";
-
-    let myRequest = new Request(endpoint, options);
-
-    fetch(myRequest)
-        .then(response => {
-            return response.json();
-        }).then(data => {
-
-            if (data) {
-
-                 // Hide Loading Icon
-                document.getElementsByClassName('hanoi-score-list')[0].innerHTML = '';
-
-                // Render Data              
-                for (var key in data) {
-                    let row = "<div class='row'><div class='col-md-3'>" + data[key].name + "</div><div class='col-md-3'>" + data[key].disks + " Disc</div><div class='col-md-3'>" + data[key].moves + " Movs</div><div class='col-md-3'>" + data[key].time + "</div></div><hr>"
-                    document.getElementsByClassName('hanoi-score-list')[0].innerHTML += row;                 
-                }
-            }
-
-        }).catch(err => console.log(err));
+function getScores(newRecord = null) {
+    var ref = firebase.database().ref('/scores/'+ hanoiTowersGame.quantDisks);    
+    
+    ref.orderByChild("moves").on("child_added", function (data) {
+        let row = "<div class='row'><div class='col-md-3'>" + data.val().name + "</div><div class='col-md-3'>" + data.val().disks + " Disc</div><div class='col-md-3'>" + data.val().moves + " Movs</div><div class='col-md-3'>" + data.val().time + "</div></div><hr>"
+        document.getElementsByClassName('hanoi-score-list')[0].innerHTML += row;       
+    });
 }
 
 /**
  * Firebase POST Score
  * @param {*} data 
  */
-function postScore(data = null) {
+function postScore(data) {
+
+    var ref = firebase.database().ref('/scores/'+ hanoiTowersGame.quantDisks);
+
+    ref.push(
+        data
+    );
+
     
-    // Keep Original data
-    var dataRender = data;
+    // Clean Form
+    document.getElementsByClassName('form-score')[0].innerHTML = null;
+    
+    // Clean List
+    document.getElementsByClassName('hanoi-score-list')[0].innerHTML = "";
 
-    data = JSON.stringify(data);   
-
-    let headers = new Headers();
-    headers.append("Content-Type", "application/json");
-
-    let options = {
-        headers: headers,
-        method: "POST",
-        body: data,
-        cache: 'default'
-    };
-
-    // Build endpoint
-    let endpoint = firebaseUrl + "/" + hanoiTowersGame.quantDisks +".json";
-
-    let myRequest = new Request(endpoint, options);
-
-    fetch(myRequest)
-        .then(response => {
-            return response.json();
-        }).then(data => {
-
-            // If response ID
-            if (data) {
-                // Render Data
-                let row = "<div class='row'><div class='col-md-3'>" + dataRender.name + "</div><div class='col-md-3'>" + dataRender.disks + " Disc</div><div class='col-md-3'>" + dataRender.moves + " Movs</div><div class='col-md-3'>" + dataRender.time + "</div></div><hr>"
- 
-                document.getElementsByClassName('hanoi-score-new-record')[0].innerHTML += row;
-        
-                // Clean Form
-                document.getElementsByClassName('form-score')[0].innerHTML = null;
-                
-            }
-
-        }).catch(err => console.log(err));
+    // Reload Data
+    getScores();
 }
+
 
 
 
