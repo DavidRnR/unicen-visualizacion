905522c1ef5360db87da16938fe19cbc997e59da
David Martín <dmartin@beerealit.com>
2017-11-02 11:06:32 -0300

    Carousel implemented with MockPic API

--- TP5/EntregaFinal/js/loader.js
+++ TP5/EntregaFinal/js/loader.js
@@ -1,8 +1,105 @@
+// NavBar Buttons
 var buttonAbout = document.getElementById('about');
 buttonAbout.addEventListener('click', (e) => {
     e.preventDefault();
     renderHtml('html/about.html');
 });
+
+var photos;
+var urlFullImage = "";
+var divCarousel;
+var imgCarousel;
+
+// Button Search
+$(document).on("click", '#search-button', (e) => {
+    e.preventDefault();
+    renderHtml('html/carousel.html');
+
+    loadMockPictures();
+});
+
+function loadMockPictures() {
+
+    // divCarousel = document.getElementsByClassName('carousel-img-container')[0];
+    // imgCarousel = document.getElementById('carousel-img');
+
+    // Just to try render pictures on the carousel
+    // TODO replace for TWITTER Pictures - API
+    fetch('https://jsonplaceholder.typicode.com/photos').then(resp => {
+        return resp.json();
+    }).then((data) => {
+        console.log(data);
+        for (var index = 0; index < 3; index++) {
+
+            photos = data;
+            urlFullImage = photos[0].url;
+        
+
+            // Set divCarousel & imgCarousel
+            divCarousel = $('.carousel-img-container')[0];
+            imgCarousel = $('#carousel-img')[0];
+ 
+            // Set Picture on the DOM
+            setPictureOnCarousel(urlFullImage);
+        }
+    });
+}
+
+/**
+ * Set Picture on the Carousel and Set Animation
+ * @param {*} url 
+ * @param {*} arrow 
+ */
+function setPictureOnCarousel(url, arrow = null) {
+
+    $(divCarousel).removeClass("slide-pic-back");
+    $(divCarousel).removeClass("slide-pic-next");
+
+    setTimeout(() => {
+        divCarousel.style.backgroundImage = 'url("' + url + '")';
+        imgCarousel.src = url;
+        $(divCarousel).addClass("slide-pic-" + ((arrow) ? arrow : 'next'));
+    }, 100);
+
+}
+
+// Button Next Image - Carousel
+$(document).on("click", '#get-next-img', (e) => {
+    e.preventDefault();
+
+    for (let i = 0; i < 10; i++) {
+
+        if (urlFullImage == photos[i].url) {
+            if (i + 1 == 10) {
+                return;
+            }
+            urlFullImage = photos[i + 1].url;
+            // Set Picture on the DOM
+            setPictureOnCarousel(urlFullImage, 'next');
+            return;
+        }
+    }
+});
+
+
+// Button Back Image - Carousel
+$(document).on("click", '#get-back-img', (e) => {
+    e.preventDefault();
+
+    for (let i = 0; i < 10; i++) {
+        if (urlFullImage == photos[i].url) {
+            if (i - 1 == -1) {
+                return;
+            }
+            urlFullImage = photos[i - 1].url;
+            // Set Picture on the DOM
+            setPictureOnCarousel(urlFullImage, 'back');
+            return;
+        }
+    }
+
+});
+
 
 /**
  * On load page, render the menu
@@ -29,15 +126,9 @@ function renderHtml(url) {
             return response.text();
         }).then(data => {
 
-            switch (url) {
-                case 'html/home.html':
-                    document.getElementById('app-loader').innerHTML = data;
-                    break;
-                case 'html/about.html':
-                    document.getElementById('app-loader').innerHTML = data;
-                    break;
-                default:
-                    break;
-            }
+            // Render Data
+            document.getElementById('app-loader').innerHTML = data;
+
         }).catch(err => console.log(err));
 }
+
