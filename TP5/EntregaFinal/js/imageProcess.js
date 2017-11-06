58eac73aaf205dda5ee88704a52e193760c80597
David Martín <dmartin@beerealit.com>
2017-11-06 13:40:59 -0300

    CSS Fixed. Buttons to display Cards or Carousel implemented

--- TP5/EntregaFinal/js/imageProcess.js
+++ TP5/EntregaFinal/js/imageProcess.js
@@ -1,6 +1,9 @@
-
+/**
+ * Get All Tweets
+ * @param {*} hash 
+ */
 function retrieveTweets(hash) {
-  getTweets(hash).then((resp)=>{
+  getTweets(hash).then((resp) => {
     var picsArray = [];
     for (var i = 0; i < resp.statuses.length; i++) {
       var tweet = resp.statuses[i];
@@ -15,18 +18,50 @@ function retrieveTweets(hash) {
       }
     }
     photos = picsArray;
+    
+    // createCarousel();
+  
     createCards();
+    // Show Spinner
+    showHideLoadingSpinner();
+
   });
 }
 
+/**
+ * Show or Hide Loadibg Spinner
+ */
+function showHideLoadingSpinner() {
+  let loadingSpinner = document.getElementsByClassName('loading-spinner')[0];
+
+  loadingSpinner.style.display = (loadingSpinner.style.display == 'none') ? 'initial' : 'none';
+}
+
+/**
+ * Create Gallery Cards
+ */
 function createCards() {
   for (var index = 0; index < photos.length; index++) {
-      let div = document.createElement('div');
-      div.className = "cards-view col-md-3";
-      div.style.backgroundImage = 'url("'+ photos[index] +'")';
-      let img = new Image();
-      img.src = photos[index];
-      div.appendChild(img);
-      $('#cards-view')[0].append(div);
+    let div = document.createElement('div');
+    div.className = "cards-view col-md-3";
+    div.style.backgroundImage = 'url("' + photos[index] + '")';
+    let img = new Image();
+    img.src = photos[index];
+    div.appendChild(img);
+    $('#cards-view')[0].append(div);
   }
+}
+
+/**
+ * Create Carousel
+ */
+function createCarousel() {
+  
+  urlFullImage = photos[0];
+  // Set divCarousel & imgCarousel
+  divCarousel = $('.carousel-img-container')[0];
+  imgCarousel = $('#carousel-img')[0];
+
+  // Set Picture on the DOM
+  setPictureOnCarousel(urlFullImage);
 }
